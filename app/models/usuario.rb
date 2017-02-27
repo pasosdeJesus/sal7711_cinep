d78b344c76982ee207fa750e15612c54df3886e3
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-02-27 11:19:02 -0500

    longitud de nusuario en 255

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -5,6 +5,4 @@ class Usuario < Sal7711Gen::Usuario
   campofecha_localizado :confirmed_at
   campofecha_localizado :fecharenovacion
   campofecha_localizado :locked_at
-
-  validates_length_of :nusuario, maximum: 255
 end
