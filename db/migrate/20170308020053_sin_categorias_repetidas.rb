d8f9d3f287c8898267cdc5d9039ec889034a1bca
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-03-09 10:11:27 -0500

    corrige mas

--- db/migrate/20170308020053_sin_categorias_repetidas.rb
+++ db/migrate/20170308020053_sin_categorias_repetidas.rb
@@ -3,16 +3,14 @@ class SinCategoriasRepetidas < ActiveRecord::Migration[5.0]
   def up
     execute <<-EOF
       UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Seguridad ciudadana, milicias urbanas, bandas y pandillas, pactos en Bogotá' WHERE codigo='B65';
-      UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Marchas' WHERE codigo='D55';
-      UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Marchas Campesinas' WHERE codigo='D75';
-      UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Marchas Laborales' WHERE codigo='D93';
+      UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Marchas campesinas' WHERE codigo='D75';
+      UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Marchas laborales' WHERE codigo='D93';
     EOF
   end
 
   def down
     execute <<-EOF
       UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Seguridad ciudadana, milicias urbanas, bandas y pandillas, pactos' WHERE codigo='B65';
-      UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Marchas' WHERE codigo='D55';
       UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Marchas' WHERE codigo='D75';
       UPDATE sal7711_gen_categoriaprensa SET NOMBRE='Marchas' WHERE codigo='D93';
     EOF
