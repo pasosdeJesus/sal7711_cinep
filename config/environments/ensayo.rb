a016f537744b690518d3be0f952667e2813bf336
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-10-14 16:01:44 -0500

    presenta bien texto de un articulo cuando no se busca texto

--- config/environments/ensayo.rb
+++ config/environments/ensayo.rb
@@ -18,8 +18,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  #config.assets.compress = true
+  #config.assets.js_compressor = NoCompression.new
+  #config.assets.css_compressor = NoCompression.new
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
