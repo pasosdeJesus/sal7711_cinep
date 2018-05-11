c55a273eb28c2e2d99b6d0d7ca3a0d53d0651fe5
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-05-11 17:27:42 -0500

    autentica por IP

--- app/controllers/sal7711_gen/buscar_controller.rb
+++ app/controllers/sal7711_gen/buscar_controller.rb
@@ -38,40 +38,39 @@ module Sal7711Gen
     end
 
     def autentica_especial
-      return false
-#      puts "OJO ipx, request=", request.inspect;
-#      org = ::Organizacion.joins(:ip_organizacion)
-#        .where('?<<=ip', request.remote_ip).take
-#      if !current_usuario
-#        nips = ::IpOrganizacion.where('? <<= ip', request.remote_ip).
-#          count('organizacion_id', distinct: true)
-#        if nips === 0
-#          if (current_usuario && ::Organizacion.
-#              where('usuarioip_id=?', current_usuario.id).
-#              count('*') > 0)
-#            # Si la organización ya no se autentica por IP se termina
-#            # sesión de usuario
-#            sign_out(current_usuario)
-#          end
-#          return false
-#        elsif nips > 1
-#          ::Ability::ultimo_error_aut = 'IP coincide con varias organizaciones'
-#          puts "** Error: ", ::Ability::ultimo_error_aut 
-#          return false
-#        else
-#          if !org.usuarioip_id
-#            ::Ability.ultimo_error_aut = 'Organización sin Usuario IP'
-#            puts "** Error: ", ::Ability::ultimo_error_aut 
-#            return false
-#          end
-#          us = ::Usuario.find(org.usuarioip_id)
-#          sign_in(us) #, bypass: true#, store: false
-#          #byebug
-#          current_usuario.autenticado_por_ip = true
-#          current_usuario.save!
-#          return true;
-#        end
-#      end
+      puts "OJO ipx, request=", request.inspect;
+      norgs = ::Organizacion.joins(:iporganizacion).
+          where('? <<= ip', request.remote_ip).count
+      if !current_usuario
+        if norgs === 0
+          if (current_usuario && ::Organizacion.
+              where('usuarioip_id=?', current_usuario.id).
+              count('*') > 0)
+            # Si la organización ya no se autentica por IP se termina
+            # sesión de usuario
+            sign_out(current_usuario)
+          end
+          return false
+        elsif norgs > 1
+          ::Ability::ultimo_error_aut = 'IP coincide con varias organizaciones'
+          puts "** Error: ", ::Ability::ultimo_error_aut 
+          return false
+        else
+          org = ::Organizacion.joins(:iporganizacion)
+          .where('?<<=ip', request.remote_ip).take
+          if !org.usuarioip_id
+            ::Ability.ultimo_error_aut = 'Organización sin Usuario IP'
+            puts "** Error: ", ::Ability::ultimo_error_aut 
+            return false
+          end
+          us = ::Usuario.find(org.usuarioip_id)
+          sign_in(us) #, bypass: true#, store: false
+          #byebug
+          current_usuario.autenticado_por_ip = true
+          current_usuario.save!
+          return true;
+        end
+      end
 #      if current_usuario && current_usuario.autenticado_por_ip
 #        if org.opciones_url_nombre_cif
 #          Rails.configuration.action_mailer.default_url_options[:host] = 
