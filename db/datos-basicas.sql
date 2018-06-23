c5e1a67b17a47ef1ff20768937d85a39892d44ea
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-06-22 19:45:56 -0500

    semillas

--- db/datos-basicas.sql
+++ db/datos-basicas.sql
@@ -1,2 +1,3 @@
-INSERT INTO organizacion (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at, autoregistro, dominiocorreo, pexcluyecorreo, diasvigencia, fecharenovacion) VALUES (1, 'CINEP', 'CENTRO DE INVESTIGACIÓN Y EDUCACIÓN POPULAR', '2015-05-07', NULL, '2015-05-07 19:48:29.158789', '2015-05-07 19:48:29.158789', true, 'cinep.org.co', 'pasante.*', '36500', '2017-09-20');
+
+INSERT INTO organizacion (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at, autoregistro, dominiocorreo, pexcluyecorreo, usuarioip_id) VALUES (1, 'CINEP', 'CENTRO DE INVESTIGACIÓN Y EDUCACIÓN POPULAR', '2015-05-07', NULL, '2015-05-07 19:48:29.158789', '2015-05-07 19:48:29.158789', true, 'cinep.org.co', 'pasante.*', 1);
 
