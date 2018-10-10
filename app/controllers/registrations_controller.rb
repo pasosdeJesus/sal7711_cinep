7d0876c774d9d02442dfdfa410dc9bd9bc1d2bb1
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-10-10 14:58:46 -0500

    Resuelve problema en autoregistro

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -18,15 +18,15 @@ class ::RegistrationsController < Devise::RegistrationsController
 
   def create
     d = ::Mail::Address.new(params[:usuario][:email]).domain
-    if Organizacion.where(dominiocorreo: d).count != 1 then
-      set_flash_message :error, :correo_desconocido
-      clean_up_passwords resource
-      respond_with self.resource, location: '/'
-    else
-      #org = Organizacion.where(dominiocorreo: d).take
+    if Organizacion.where(dominiocorreo: d).count == 1 && 
+      Organizacion.where(dominiocorreo: d).take.autoregistro then
       params[:usuario][:nusuario] = params[:usuario][:email].gsub(/[@.]/,"_")
       params[:usuario][:fechacreacion] = Date.today
       super
+    else
+      set_flash_message :error, :correo_desconocido
+      clean_up_passwords resource
+      respond_with self.resource, location: '/'
     end
     #byebug
   end
