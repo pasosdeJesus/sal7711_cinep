d9b589adae69c99a15caa5e81a807f6a6c26f004
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-03-19 13:48:13 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
-# Hace pruebas, pruebas de regresión y envia a github
+# Hace pruebas, pruebas de regresión, envia a github y sube a heroku
 
-if (test -f .env) then {
+if (test -f ".env") then {
 	. ./.env
 } fi;
-grep "^ *gem *.sip.*, *path:" Gemfile > /dev/null 2> /dev/null
+
+grep "^[^#]*path" -B 1 -A 1 Gemfile
 if (test "$?" = "0") then {
-	echo "Gemfile incluye un sip cableado al sistema de archivos"
-	exit 1;
-} fi;
-grep "^ *gem *.sal7711_gen.*, *path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un sal7711_gen cableado al sistema de archivos"
-	exit 1;
+  echo "Hay una gema cableada en el sistema de archivos"
+  exit 1;
 } fi;
 
 grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile incluye debugger que heroku no quiere"
+	echo "Gemfile incluye debugger"
 	exit 1;
 } fi;
 grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
@@ -47,40 +43,52 @@ if (test "$SININS" != "1") then {
 	} fi;
 } fi;
 if (test "$SINMIG" != "1") then {
-	(bin/rails db:migrate sip:indices db:structure:dump)
+        PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY bundle exec rake db:migrate sip:indices db:structure:dump
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
 
-(RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
+PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY RAILS_ENV=test bundle exec rake db:drop db:setup;  PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY RAILS_ENV=test bin/rails db:migrate sip:indices
 if (test "$?" != "0") then {
 	echo "No puede preparse base de prueba";
 	exit 1;
 } fi;
 
-bundle exec rails test
+PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY bundle exec bin/rails test
 if (test "$?" != "0") then {
 	echo "No pasaron pruebas";
 	exit 1;
 } fi;
-CONFIG_HOSTS=127.0.0.1 bundle exec rails test:system
+
+
+CONFIG_HOSTS=127.0.0.1 PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY bundle exec bin/rails test:system
 if (test "$?" != "0") then {
-	echo "No pasaron pruebas del sistema";
+	echo "No pasaron pruebas de sistema";
 	exit 1;
 } fi;
 
-
-(RAILS_ENV=test bin/rails db:structure:dump)
+RAILS_ENV=test bin/rails db:structure:dump
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
 if (test "$MENSCONS" = "") then {
-	git commit -a
-} else {
-	git commit -m $MENSCONS -a
+	MENSCONS="Actualiza"
 } fi;
+git commit -m "$MENSCONS" -a
 git push origin ${b}
 if (test "$?" != "0") then {
 	echo "No pudo subirse el cambio a github";
 	exit 1;
 } fi;
+
+
+if (test "$CONH" != "") then {
+	git push heroku master
+	heroku run rake db:migrate sip:indices
+} fi;
+
+if (test "$?" != "0") then {
+	echo "No puede preparse base de prueba";
+	exit 1;
+} fi;
+
