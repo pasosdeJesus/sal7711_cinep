3987b26c15a3fbcfdfca41e3bd8439b99df52cc8
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-04-26 10:10:35 -0500

    Ajusta para produccion

--- bin/u.sh
+++ bin/u.sh
@@ -8,12 +8,17 @@ if (test "${USUARIO_AP}" = "") then {
 	echo "Definir usuario con el que se ejecuta en USUARIO_AP"
 	exit 1;
 } fi;
+if (test "$PGSSLCERT" = "") then {
+	echo "Falta PGSSLCERT"
+	exit 1;
+} fi;
+if (test "$PGSSLKEY" = "") then {
+	echo "Falta PGSSLKEY"
+	exit 1;
+} fi;
 DOAS=`which doas`
 if (test "$DOAS" = "") then {
 	DOAS=sudo
 } fi;
-$DOAS su ${USUARIO_AP} -c "cd /var/www/htdocs/sal7711_cinep; RAILS_ENV=production bundle exec rake assets:precompile; echo \"Iniciando unicorn...\"; SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c ../sal7711_cinep/config/unicorn.conf.minimal.rb  -E production -D"
-
-
-  
+$DOAS su - ${USUARIO_AP} -c "cd /var/www/htdocs/sal7711_cinep; PGSSLCERT=${PGSSLCERT} PGSSLKEY=${PGSSLKEY} RAILS_ENV=production bin/rails assets:precompile; echo \"Iniciando unicorn...\"; PGSSLCERT=${PGSSLCERT} PGSSLKEY=${PGSSLKEY} SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c ../sal7711_cinep/config/unicorn.conf.minimal.rb  -E production -D"
 
