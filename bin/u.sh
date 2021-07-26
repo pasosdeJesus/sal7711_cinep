df8cc470b12fe79bfa3a7088715c7d608c032617
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-07-26 15:40:26 -0500

    Mejorando modo ensayo

--- bin/u.sh
+++ bin/u.sh
@@ -1,24 +1,67 @@
 #!/bin/sh
 # Inicia produccion
+
+if (test "${DIRAP}" = "") then {
+  echo "Definir directorio de aplicacion en DIRAP"
+  exit 1;
+} fi;
+
+if (test -f "${DIRAP}/.env") then {
+  . $DIRAP/.env
+} fi;
+
 if (test "${SECRET_KEY_BASE}" = "") then {
-	echo "Definir variable de ambiente SECRET_KEY_BASE"
-	exit 1;
+  echo "Definir variable de ambiente SECRET_KEY_BASE"
+  exit 1;
 } fi;
 if (test "${USUARIO_AP}" = "") then {
-	echo "Definir usuario con el que se ejecuta en USUARIO_AP"
-	exit 1;
+  echo "Definir usuario con el que se ejecuta en USUARIO_AP"
+  exit 1;
 } fi;
-if (test "$PGSSLCERT" = "") then {
-	echo "Falta PGSSLCERT"
-	exit 1;
+#if (test "${RAILS_RELATIVE_URL_ROOT}" = "") then {
+#  echo "Definir ruta relativa en URL en RAILS_RELATIVE_URL_ROOT"
+#  exit 1;
+#} fi;
+
+if (test "${PGSSLCERT}" = "") then {
+  PGSSLCERT=~/.postgresql/postgresql.crt
 } fi;
-if (test "$PGSSLKEY" = "") then {
-	echo "Falta PGSSLKEY"
-	exit 1;
+if (test "${PGSSLKEY}" = "") then {
+  PGSSLKEY=~/.postgresql/postgresql.key
 } fi;
-DOAS=`which doas`
+
+DOAS=`which doas 2>/dev/null`
 if (test "$DOAS" = "") then {
-	DOAS=sudo
+  DOAS=sudo
 } fi;
-$DOAS su - ${USUARIO_AP} -c "cd /var/www/htdocs/sal7711_cinep; PGSSLCERT=${PGSSLCERT} PGSSLKEY=${PGSSLKEY} RAILS_ENV=production bin/rails assets:precompile; echo \"Iniciando unicorn...\"; PGSSLCERT=${PGSSLCERT} PGSSLKEY=${PGSSLKEY} SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c ../sal7711_cinep/config/unicorn.conf.minimal.rb  -E production -D"
+
+echo "RAILS_ENV=$RAILS_ENV"
+defuroot=""
+if (test "${RAILS_RELATIVE_URL_ROOT}" != "") then {
+  defuroot="RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT}"
+} fi;
+
+$DOAS su - ${USUARIO_AP} -c "cd $DIRAP; 
+  echo \"== Iniciando unicorn... ==\"; 
+  ${defuroot} PUERTOUNICORN=${PUERTOUNICORN} \
+    CONFIG_HOSTS=${CONFIG_HOSTS}\
+    DIRAP=$DIRAP \
+    RAILS_ENV=${RAILS_ENV} \
+    SECRET_KEY_BASE=${SECRET_KEY_BASE} \
+    BD_SERVIDOR=${BD_SERVIDOR} \
+    BD_CLAVE=${BD_CLAVE} \
+    BD_USUARIO=${BD_USUARIO} \
+    BD_ENSAYO=${BD_ENSAYO} \
+    BD_PRO=${BD_PRO} \
+    SMTP_MAQ=${SMTP_MAQ}  \
+    SMTP_DOMINIO=${SMTP_DOMINIO} \
+    SMTP_PUERTO=${SMTP_PUERTO} \
+    SMTP_CLAVE=${SMTP_CLAVE} \
+    SMTP_USUARIO=${SMTP_USUARIO} \
+    PGSSLCERT=${PGSSLCERT} \
+    PGSSLKEY=${PGSSLKEY} \
+    RUTA_RELATIVA=${RUTA_RELATIVA} \
+    HEB412_RUTA=${HEB412_RUTA} \
+    bundle exec /usr/local/bin/unicorn_rails \
+    -c $DIRAP/config/unicorn.conf.minimal.rb  -E $RAILS_ENV -D"
 
